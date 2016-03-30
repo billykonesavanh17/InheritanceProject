1710ec16b920b34b7cd8edd4d54f163602d3227f
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-03-30 08:01:39 -0600

    added setters and getters

--- src/ctec/model/AudiCar.java
+++ src/ctec/model/AudiCar.java
@@ -12,4 +12,34 @@ public class AudiCar extends Car
 		this.setYearMake(2017);
 		this.setBrandName("Audi");
 	}
+	
+	public String getModelName()
+	{
+		return modelName;
+	}
+
+	public void setModelName(String modelName)
+	{
+		this.modelName = modelName;
+	}
+
+	public int getYearMake()
+	{
+		return yearMake;
+	}
+
+	public void setYearMake(int yearMake)
+	{
+		this.yearMake = yearMake;
+	}
+
+	public String getBrandName()
+	{
+		return brandName;
+	}
+
+	public void setBrandName(String brandName)
+	{
+		this.brandName = brandName;
+	}
 }
--- src/ctec/model/NissanCar.java
+++ src/ctec/model/NissanCar.java
@@ -12,4 +12,36 @@ public class NissanCar extends Car
 		this.setYearMake(2002);
 		this.setBrandName("Nisan");
 	}
+
+	public String getModelName()
+	{
+		return modelName;
+	}
+
+	public void setModelName(String modelName)
+	{
+		this.modelName = modelName;
+	}
+
+	public int getYearMake()
+	{
+		return yearMake;
+	}
+
+	public void setYearMake(int yearMake)
+	{
+		this.yearMake = yearMake;
+	}
+
+	public String getBrandName()
+	{
+		return brandName;
+	}
+
+	public void setBrandName(String brandName)
+	{
+		this.brandName = brandName;
+	}
+	
+	
 }
